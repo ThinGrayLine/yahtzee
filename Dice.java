a4152c71799af42a720a54335f80bab197ecc09b
Grayson <52610547+ThinGrayLine@users.noreply.github.com>
2022-03-09 11:53:38 -0500

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,12 +1,16 @@
-package YahtzeeProject;
-
-/**
- *
- * @author gyenc
- */
 public class Dice extends Player {
     private int diceValue;
     
+    public Dice() {
+        diceValue = (int)((Math.random() * (7-1)) + 1);
+    }
     
+    public int getDiceValue() {
+        return diceValue;
+    }
+    
+    public void setDiceValue() {
+        diceValue = (int)((Math.random() * (7-1)) + 1);
+    }
     
 }
