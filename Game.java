2391cc6a24db59a832ac27d0c15b6a1e09351ad6
Grayson <52610547+ThinGrayLine@users.noreply.github.com>
2022-03-09 10:29:00 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,31 +4,35 @@ import java.util.ArrayList;
 
 public class Game {
     public boolean game;
-    public ArrayList<Player> players; 
+    public ArrayList<Player> players;
+    private Player winner;
     
     public Game() {
         
     }
     
-    public Player setWinner() {
+    public Player isWinner(ArrayList<Player> players) {
         if (players.get(0).getTotal() > players.get(1).getTotal()) {
             // DISPLAY PLAYER 1 AS WINNER
-            players.get(0).setWinningPlayer();
+            winner = players.get(0);
             
         } else if (players.get(0).getTotal() < players.get(1).getTotal()) {
             // DISPLAY PLAYER 2 AS WINNER
-            players.get(1).setWinningPlayer();
+            winner = players.get(1);
             
         } else {
             // TIE
-            players.tie();
-            
+            // display tie?
+            return null;
         }
+        return winner; // do winner.getName();
     }
     
+    //public Player
+    /*
     public Player getWinner() {
         return players.getWinner();
-    }
+    }*/
     
     public boolean isRealGame() {
         // sees if game is active? do we even need this?
