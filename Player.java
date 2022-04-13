18b3c1f816f75abcde6230b546fff3a41d1c2e83
Grayson <52610547+ThinGrayLine@users.noreply.github.com>
2022-04-13 11:46:28 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,13 +2,15 @@ package YahtzeeProject;
 
 import java.util.ArrayList;
 
-public class Player extends Game {
+public class Player extends Game { // doesn't extend game anymore?
     //private boolean isWinner;
     //private Player winningPlayer;
     private int total;
     private String playerName;
     private int upperSectionTotal;
+    private int lowerSectionTotal;
     public ArrayList<Dice> hand;
+    private Roll roll;
     
     public Player() {
         total = 0;
@@ -20,15 +22,21 @@ public class Player extends Game {
         
     }
     
-    // SET NAME METHOD;
+    private void setPlayerName(String name) {
+        playerName = name;
+    }
+    
+    public String getPlayerName() {
+        return playerName;
+    }
     
     
     public int getTotal() {
         return total;
     } 
     
-    public void setTotal(int num) {
-        total += num;
+    public void setTotal() {
+        total = upperSectionTotal + lowerSectionTotal;
     }
     
     public int getUpperSectionTotal() {
@@ -37,9 +45,22 @@ public class Player extends Game {
     
     public void setUpperSectionTotal() {
         //upperSectionTotal CALL onesScore, twosScore... 
-        if (isOnes == false && isTwos == false...) {
-		upperSectionTotal = getOnes() + getTwos() + getThrees() + getFours() + getFives() + getSixes();
-	}
+        if (roll.isAvailableOnes() == false && roll.isAvailableTwos() == false && roll.isAvailableThrees() == false && roll.isAvailableFours() == false && roll.isAvailableFives() == false && roll.isAvailableSixes() == false) {
+		upperSectionTotal = roll.getOnes() + roll.getTwos() + roll.getThrees() + roll.getFours() + roll.getFives() + roll.getSixes();
+	} 
+        
     }
+
+    public int getLowerSectionTotal() {
+        return lowerSectionTotal;
+    }
+
+    public void setLowerSectionTotal() {
+        if (roll.isAvailableSmallStraight() == false && roll.isAvailableLargeStraight() == false && roll.isAvailableThreeOfAKind() == false && roll.isAvailableFourOfAKind() == false && roll.isAvailableFullHouse() == false && roll.isAvailableChance() == false && roll.isAvailableYahtzee() == false) {
+		lowerSectionTotal = roll.getSmallStraight() + roll.getLargeStraight() + roll.getThreeOfAKind() + roll.getFourOfAKind() + roll.getFullHouse() + roll.getChance() + roll.getYahtzee();
+	} 
+    }
+    
+    
     
 }
