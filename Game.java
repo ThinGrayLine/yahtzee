876be9f91eb9291feba646e7781ad328f32830aa
Grayson <52610547+ThinGrayLine@users.noreply.github.com>
2022-04-13 11:46:58 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -3,14 +3,14 @@ package YahtzeeProject;
 import java.util.ArrayList;
 
 public class Game {
-    public boolean game;
-    public ArrayList<Player> players;
-    private Player winner;
+    //public boolean game;
+    //public ArrayList<Player> players;
+    //private Player winner;
     
     public Game() {
         
     }
-    
+    /*
     public Player isWinner(ArrayList<Player> players) {
         if (players.get(0).getTotal() > players.get(1).getTotal()) {
             // DISPLAY PLAYER 1 AS WINNER
@@ -27,15 +27,16 @@ public class Game {
         }
         return winner; // do winner.getName();
     }
-    
+    */
     //public Player
     /*
     public Player getWinner() {
         return players.getWinner();
     }*/
-    
+    /*
     public boolean isRealGame() {
         // sees if game is active? do we even need this?
     }
+    */
     
 }
