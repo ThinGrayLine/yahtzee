3e5c761cfcd28b6db5af6c4f04b70ee6c19aa240
Grayson <52610547+ThinGrayLine@users.noreply.github.com>
2022-04-18 22:55:42 -0400

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -1,5 +1,6 @@
 public class Dice extends Player {
     private int diceValue;
+    private boolean selected;
     
     public Dice() {
         diceValue = (int)((Math.random() * (7-1)) + 1);
@@ -11,6 +12,14 @@ public class Dice extends Player {
     
     public void setDiceValue() {
         diceValue = (int)((Math.random() * (7-1)) + 1);
+    }
+    
+    public boolean isSelected() {
+        return selected;
+    }
+    
+    public void setSelected(boolean value) {
+        selected = value;
     }
     
 }
